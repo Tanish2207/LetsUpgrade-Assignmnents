55a4f6e6d0964888eb20ba533c2bd1a7cce171a8
Tanish2207 <71320858+Tanish2207@users.noreply.github.com>
2021-01-23 16:02:22 +0530

    Assignments submitted Day2

---
+++ Javascript Ess JAN 2021 Batch 1/Day 3 Assgn 2/Q1.js
@@ -0,0 +1,10 @@
+const clothes = ['jacket', 't-shirt'];
+clothes.length = 0;
+console.log(clothes[1]);
+
+// The length property of JS Arrays gives us the total
+// no. elements in the array. But here, we are specifying that 
+// the length of the array is 0, it means that we are telling that 
+// there are 0 elements in the array. Hence the array becomes empty.
+
+// Therefore, the index 1 of clothes array is UNDEFINED.
---
+++ Javascript Ess JAN 2021 Batch 1/Day 3 Assgn 2/Q2.js
@@ -0,0 +1,8 @@
+arr = [23, 20, 1, 4, 20] // Defining an array
+
+sum = 0;             // storing the sum in a variable
+
+for(var i = 0; i < arr.length; i++) { // For loop
+    sum += arr[i]
+}
+console.log(sum);
